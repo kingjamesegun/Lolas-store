28ba8d949562064facd9f3a6225ff7a2ba6f8f9f
James <kingjamesegun@gmail.com>
2022-03-09 20:52:36 +0100

    Peace Error Fixed

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,26 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <div className=''>
-      <Head>
-        <title>Lolas Store</title>
-        <meta name="description" content="Lolas store is a web store which allows you to create your own store on the site and sell your goods and services." />
-        <link rel="icon" href="images/LogoFavicon.png" />
-      </Head>
+	return (
+		<div className=''>
+			<Head>
+				<title>Lolas Store</title>
+				<meta
+					name='description'
+					content='Lolas store is a web store which allows you to create your own store on the site and sell your goods and services.'
+				/>
+				<link rel='icon' href='images/LogoFavicon.png' />
+			</Head>
 
-    <main className='flex flex-col items-center pt-4'>
-      <Link href='./login'><a>Login Page</a></Link>
-      <Link href='./sign-up'><a>Sign up Page</a></Link>
-    </main>
-    </div>
-  )
+			<main className='flex flex-col items-center pt-4'>
+				<Link href='./login'>
+					<a>Login Page</a>
+				</Link>
+				<Link href='./sign-up'>
+					<a>Sign up Page</a>
+				</Link>
+			</main>
+		</div>
+	);
 }
