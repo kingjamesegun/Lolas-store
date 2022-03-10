286e2b8523f5c4ae96db10a7b038a1e9e389df5f
Pappyjay23 <pjinadu23@gmail.com>
2022-03-10 14:12:35 +0100

    Fixed the height not being full

--- pages/sign-up/index.js
+++ pages/sign-up/index.js
@@ -10,7 +10,7 @@ const signUp = () => {
         <meta name="description" content="Sign up page to be able to create your account" />
         <link rel="shortcut icon" href="images/LogoFavicon.png" type="image/x-icon" />
       </Head>
-      <section className="font-Roboto flex flex-col md:flex-row md:flex-1 lg:h-screen h-full">
+      <section className="font-Roboto flex flex-col md:flex-row md:flex-1 h-full md:h-full">
         <div className="pb-6 p-4 w-full md:w-6/12 lg:p-16 md:p-8">
           <div className="mb-8">
             <h1 className="font-semibold text-input-border lg:text-4xl text-2xl sm:text-3xl mb-1 tracking-normal">Create Account</h1>
