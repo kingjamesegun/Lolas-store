a5808fde32a0d95d46d963f25f08d4dfa6da0d39
Tolulope <emryzs01@gmail.com>
2022-03-14 18:24:23 +0300

    added the dashboard template

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Sidebar from '../components/Sidebar';
 import NestedLayout from "../components/NestedLayout";
+import Navbar from '../components/Navbar';
 
 export default function Dashboard() {
+    const [closed,setClosed] = React.useState(true);
     return (
-        <div>Dashboard</div>)
+        <div className="h-screen bg-[#E5E7EB] flex mx-auto max-w-screen-2xl">
+          {
+            closed && 
+            <section className="mr-1 hidden transform duration-150 ease-in-out lg:flex">
+              <Sidebar/>
+            </section>
+          }
+          <section className="flex-grow">
+            <Navbar handleClose={()=>setClosed(!closed)}/>
+            <main className='p-1'>
+              {/* main content comes here */}
+              Content here
+            </main>
+          </section>
+        </div>
+    )
   }
   
 Dashboard.getLayout = function getLayout(page) {
